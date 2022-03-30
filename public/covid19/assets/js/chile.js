99da87282cd2fbf6f0f01455641eeb1ab3a03fb4
melaniepardo <melaniepardoes@gmail.com>
2022-03-30 20:09:45 -0300

    actualizacion mel grafico

--- public/covid19/assets/js/chile.js
+++ public/covid19/assets/js/chile.js
@@ -9,17 +9,60 @@ const menuItemLogoutSelector = document.querySelector('#menu-item-logout')
 const botonLoginSelector = document.querySelector('#boton-login')
 const botonLogoutSelector = document.querySelector('#boton-logout')
 
+
 const iniciarSesionModal = new bootstrap.Modal(document.querySelector('#modal-principal'), {})
+
 
 // ocultar elementos del menu (situción chile, cerrar sesion)
 menuItemSituacionChileSelector.style.display = "none"
 menuItemLogoutSelector.style.display = "none"
 
+const mostrarGrafico = (data) => {
+    new Chart(document.getElementById("grafico"), {
+        type: 'line',
+        data: {
+            labels: [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050],
+            datasets: [{
+                data: [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478],
+                label: "Confimados",
+                borderColor: "#3e95cd",
+                fill: false
+            }, {
+                data: [282, 350, 411, 502, 635, 809, 947, 1402, 3700, 5267],
+                label: "Muertos",
+                borderColor: "#8e5ea2",
+                fill: false
+            }, {
+                data: [168, 170, 178, 190, 203, 276, 408, 547, 675, 734],
+                label: "Europe",
+                borderColor: "#3cba9f",
+                fill: false
+            }, {
+                data: [40, 20, 10, 16, 24, 38, 74, 167, 508, 784],
+                label: "Latin America",
+                borderColor: "#e8c3b9",
+                fill: false
+            }, {
+                data: [6, 3, 2, 2, 7, 26, 82, 172, 312, 433],
+                label: "North America",
+                borderColor: "#c45850",
+                fill: false
+            }
+            ]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'World population per region (in millions)'
+            }
+        }
+    });
+}
 const postData = async (email, password) => {
     try {
         const response = await fetch("http://localhost:3000/api/login", // Consulta para crear token
             {
-                method: 'POST', // Crear el token 
+                method: 'POST', // Crear el token
                 body: JSON.stringify({ email, password }),
             })
         const { token } = await response.json()
@@ -105,9 +148,13 @@ formularioSelector.addEventListener("submit", async (event) => {
     // ocultando item menu (iniciar sesion)
     menuItemLoginSelector.style.display = "none"
     // llamado asincrono de Apis
-    await consumirDatosApiConfirmed()
-    await consumirDatosApiRecovered()
-    await consumirDatosApiDeaths()
+    const resultadosConfirmed = await consumirDatosApiConfirmed()
+    const resultadosRecovered = await consumirDatosApiRecovered()
+    const resultadosDeaths = await consumirDatosApiDeaths()
+    console.log(resultadosConfirmed)
+    console.log(resultadosRecovered)
+    console.log(resultadosDeaths)
+    mostrarGrafico(data)
 })
 
 botonLoginSelector.addEventListener("click", () => {
@@ -121,7 +168,4 @@ botonLogoutSelector.addEventListener("click", () => {
     menuItemLoginSelector.style.display = "list-item"
 })
 
-//const manejadorClick = () => {
-
-
-
+// const manejadorClick = () => {
