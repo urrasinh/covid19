89cbfe6191cea386aa5544dc084c1dc1fdbc35ed
melaniepardo <melaniepardoes@gmail.com>
2022-03-30 22:12:55 -0300

    actualizacion quitar grafico Chile final

--- public/covid19/assets/js/chile.js
+++ public/covid19/assets/js/chile.js
@@ -8,7 +8,7 @@ const menuItemLoginSelector = document.querySelector('#menu-item-login')
 const menuItemLogoutSelector = document.querySelector('#menu-item-logout')
 const botonLoginSelector = document.querySelector('#boton-login')
 const botonLogoutSelector = document.querySelector('#boton-logout')
-
+const cerrarGraficoSelector = document.querySelector('#grafico')
 
 const iniciarSesionModal = new bootstrap.Modal(document.querySelector('#modal-principal'), {})
 
@@ -17,43 +17,36 @@ const iniciarSesionModal = new bootstrap.Modal(document.querySelector('#modal-pr
 menuItemSituacionChileSelector.style.display = "none"
 menuItemLogoutSelector.style.display = "none"
 
-const mostrarGrafico = (data) => {
+// const confirmados = resultadosConfirmed.map(p)
+
+
+const mostrarGrafico = (resultadosConfirmed, resultadosRecovered, resultadosDeaths) => {
     new Chart(document.getElementById("grafico"), {
         type: 'line',
         data: {
-            labels: [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050],
+            labels: resultadosConfirmed.map(p => p.date),
             datasets: [{
-                data: [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478],
+                data: resultadosConfirmed.map(p => p.total),
                 label: "Confimados",
                 borderColor: "#3e95cd",
                 fill: false
             }, {
-                data: [282, 350, 411, 502, 635, 809, 947, 1402, 3700, 5267],
-                label: "Muertos",
+                data: resultadosRecovered.map(p => p.total),
+                label: "Recuperados",
                 borderColor: "#8e5ea2",
                 fill: false
             }, {
-                data: [168, 170, 178, 190, 203, 276, 408, 547, 675, 734],
-                label: "Europe",
+                data: resultadosDeaths.map(p => p.total),
+                label: "Muertos",
                 borderColor: "#3cba9f",
                 fill: false
-            }, {
-                data: [40, 20, 10, 16, 24, 38, 74, 167, 508, 784],
-                label: "Latin America",
-                borderColor: "#e8c3b9",
-                fill: false
-            }, {
-                data: [6, 3, 2, 2, 7, 26, 82, 172, 312, 433],
-                label: "North America",
-                borderColor: "#c45850",
-                fill: false
-            }
+            },
             ]
         },
         options: {
             title: {
                 display: true,
-                text: 'World population per region (in millions)'
+                text: 'Situación Covid-19 Chile (in milliones)'
             }
         }
     });
@@ -148,13 +141,15 @@ formularioSelector.addEventListener("submit", async (event) => {
     // ocultando item menu (iniciar sesion)
     menuItemLoginSelector.style.display = "none"
     // llamado asincrono de Apis
-    const resultadosConfirmed = await consumirDatosApiConfirmed()
-    const resultadosRecovered = await consumirDatosApiRecovered()
-    const resultadosDeaths = await consumirDatosApiDeaths()
+    const llamadosApi = [
+        consumirDatosApiConfirmed(), consumirDatosApiRecovered(), consumirDatosApiDeaths()
+]
+    const [resultadosConfirmed, resultadosRecovered, resultadosDeaths] = await Promise.all(llamadosApi)
     console.log(resultadosConfirmed)
     console.log(resultadosRecovered)
     console.log(resultadosDeaths)
-    mostrarGrafico(data)
+    mostrarGrafico(resultadosConfirmed, resultadosRecovered, resultadosDeaths)
+    console.log(resultadosConfirmed)
 })
 
 botonLoginSelector.addEventListener("click", () => {
@@ -166,6 +161,7 @@ botonLogoutSelector.addEventListener("click", () => {
     menuItemSituacionChileSelector.style.display = "none"
     menuItemLogoutSelector.style.display = "none"
     menuItemLoginSelector.style.display = "list-item"
+    cerrarGraficoSelector.style.display = "none"
 })
 
 // const manejadorClick = () => {
